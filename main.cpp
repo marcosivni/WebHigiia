ced4b16268932f77b972beeee33046bf45b5dc9b
marcosivni <marcosivni@gmail.com>
2022-02-22 12:20:47 -0300

    Adding support to MRI slices - Windowing

--- main.cpp
+++ main.cpp
@@ -21,8 +21,5 @@ int main(int argc, char *argv[])
         webSocket.close();
     }
 
-//    DCMImage *dcm = new DCMImage("/home/ivni/Documents/FAPESP/ESPINHA/Imgs/01-T1-S07.dcm");
-//    delete (dcm);
-
     return a.exec();
 }
